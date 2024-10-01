ffbf74e26636a4241f1e43cd7a9f80d7b0bf0bd8
lopesjao <lopesjao2503@gmail.com>
2024-09-30 21:44:17 -0300

    LOGIN DO GOOGLE FUNCIONANDO

--- View/oauth2callback.php
+++ View/oauth2callback.php
@@ -1,32 +1,69 @@
 <?php
-// Inclui as bibliotecas do Google API
-require_once 'vendor/autoload.php';
-
-// Inicializa o cliente do Google
-$client = new Google_Client();
-$client->setClientId('86253102357-79vcqcejk9ud6qp684lagut2917pcjbq.apps.googleusercontent.com');
-$client->setClientSecret('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');
-$client->setRedirectUri('http://localhost/TCC/View/oauth2callback.php'); // O mesmo URI configurado no Google Console
-$client->addScope("email");
-$client->addScope("profile");
-
-// Verifica se o código de autenticação foi enviado
-if (isset($_GET['code'])) {
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($token);
-
-    // Obtém informações do perfil do usuário
-    $oauth = new Google_Service_Oauth2($client);
-    $perfil = $oauth->userinfo->get();
-
-    // Exibe ou salva as informações do perfil do usuário
-    echo "Nome: " . $perfil->name . "<br>";
-    echo "Email: " . $perfil->email . "<br>";
-    echo "Foto de Perfil: <img src='" . $perfil->picture . "'><br>";
-    
-    // Você pode redirecionar o usuário para outra página ou salvar as informações no banco de dados
+// Initialize the session
+session_start();
+// Update the following variables
+$google_oauth_client_id = '86253102357-79vcqcejk9ud6qp684lagut2917pcjbq.apps.googleusercontent.com'; 
+$google_oauth_client_secret = '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';
+$google_oauth_redirect_uri = 'http://localhost/TCC/View/oauth2callback.php';
+$google_oauth_version = 'v3';
+// If the captured code param exists and is valid
+if (isset($_GET['code']) && !empty($_GET['code'])) {
+    // Execute cURL request to retrieve the access token
+    $params = [
+        'code' => $_GET['code'],
+        'client_id' => $google_oauth_client_id,
+        'client_secret' => $google_oauth_client_secret,
+        'redirect_uri' => $google_oauth_redirect_uri,
+        'grant_type' => 'authorization_code'
+    ];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://accounts.google.com/o/oauth2/token');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $response = json_decode($response, true);
+    // Make sure access token is valid
+    if (isset($response['access_token']) && !empty($response['access_token'])) {
+        // Execute cURL request to retrieve the user info associated with the Google account
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/oauth2/' . $google_oauth_version . '/userinfo');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $response['access_token']]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $profile = json_decode($response, true);
+        // Make sure the profile data exists
+        if (isset($profile['email'])) {
+            $google_name_parts = [];
+            $google_name_parts[] = isset($profile['given_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['given_name']) : '';
+            $google_name_parts[] = isset($profile['family_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['family_name']) : '';
+            // Authenticate the user
+            session_regenerate_id();
+            $_SESSION['google_loggedin'] = TRUE;
+            $_SESSION['google_email'] = $profile['email'];
+            $_SESSION['google_name'] = implode(' ', $google_name_parts);
+            $_SESSION['google_picture'] = isset($profile['picture']) ? $profile['picture'] : '';
+            // Redirect to profile page
+            header('Location: profile.php');
+            exit;
+        } else {
+            exit('Could not retrieve profile information! Please try again later!');
+        }
+    } else {
+        exit('Invalid access token! Please try again later!');
+    }
 } else {
-    // Redireciona o usuário para o login do Google
-    $authUrl = $client->createAuthUrl();
-    header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+    // Define params and redirect to Google Authentication page
+    $params = [
+        'response_type' => 'code',
+        'client_id' => $google_oauth_client_id,
+        'redirect_uri' => $google_oauth_redirect_uri,
+        'scope' => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+        'access_type' => 'offline',
+        'prompt' => 'consent'
+    ];
+    header('Location: https://accounts.google.com/o/oauth2/auth?' . http_build_query($params));
+    exit;
 }
